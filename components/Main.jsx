9ef8d1eba461c6fbfd237a017549883dc7b47a98
Thomi55 <thomyeze57@gmail.com>
2022-12-29 17:34:43 -0300

    portfolio

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,36 +1,46 @@
-import React from 'react';
-import {Link} from "react-scroll"
-import {MdOutlineKeyboardArrowRight} from "react-icons/md"
-import Typewriter from "typewriter-effect"
+import React from "react";
+import { Link } from "react-scroll";
+import { MdOutlineKeyboardArrowRight } from "react-icons/md";
+import Typewriter from "typewriter-effect";
 
 const Main = () => {
   return (
-    <div id='home' className='w-full h-screen text-center bg-[#256D85] to-gray-800'>
-      <div className='max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center'>
+    <div
+      id="home"
+      className="w-full h-screen text-center bg-[#256D85] to-gray-800"
+    >
+      <div className="max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center">
         <div>
-          <h1 className='py-4 text-white'>
+          <h1 className="py-4 text-white">
             Hi, my name is Thomas Ezequiel Flores
           </h1>
-          <h1 className='py-2 text-[#094157]'><Typewriter
-                        options={{
-                            strings: [
-                                "A full stack developer"
-                            ],
-                            changeDelay: 3,
-                            changeDeleteSpeed: 2,
-                            autoStart: true,
-                            loop: true,
-                        }}
-                        /></h1>
-          <p className='py-4 text-white font-bold sm:max-w-[70%] m-auto'>
-          I would like to help you in any project you need, it would be a pleasure for me to offer you my knowledge to build your idea through programming
+          <h1 className="py-2 justify-center text-[#094157] hidden sm:flex ">
+            <Typewriter
+              options={{
+                strings: ["A full stack developer"],
+                changeDelay: 3,
+                changeDeleteSpeed: 2,
+                autoStart: true,
+                loop: true,
+              }}
+            />
+          </h1>
+          <p className="py-4 text-white font-bold sm:max-w-[70%] m-auto">
+            I would like to help you in any project you need, it would be a
+            pleasure for me to offer you my knowledge to build your idea through
+            programming
           </p>
           <div>
-            <Link to="portfolio" smooth duration={500} className='w-32  mx-auto p-2 justify-center  group text-white  px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer' >
-               Portfolio
-                <span className='group-hover:rotate-90 duration-300'>
-                    <MdOutlineKeyboardArrowRight size={25}/>
-                </span>
+            <Link
+              to="portfolio"
+              smooth
+              duration={500}
+              className="w-32  mx-auto p-2 justify-center  group text-white  px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer"
+            >
+              Portfolio
+              <span className="group-hover:rotate-90 duration-300">
+                <MdOutlineKeyboardArrowRight size={25} />
+              </span>
             </Link>
           </div>
         </div>
--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,48 +1,109 @@
 import React from "react";
 import Image from "next/image";
-import pokemon from "../public/assets/images/pokemon.png"
-import kalu from "../public/assets/images/kalu.png"
+import pokemon from "../public/assets/images/pokemon.png";
+import kalu from "../public/assets/images/kalu.png";
+import videogames from "../public/assets/images/videogames.png";
 
 const Projects = () => {
-    const portfolios = [
-        {
-            id:1,
-            src: pokemon
-        },
-        {
-            id:2,
-            src: kalu
-        }
-    ]
-
-    return(
-        <div name="portfolio" className="bg-[#47B5FF] w-full text-white md:h-screen">
-            <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
-                <div className="pb-8">
-                <p className="text-4xl font-bold inline border-b-4 border-gray-500">Portfolio</p>
-                <p className="py-6">Check out some of my work right here</p>
-                </div>
-
-                <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
-                {portfolios.map(({id, src}) => (
-                    <div key={id}className="shadow-md shadow-gray-600 rounded-lg">
-                    <Image className="rounded-md duration-200 hover:scale-105" src={src} alt="/" width={500} height={100}/>
-                        <div className="flex items-center justify-center">
-                            <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">Demo</button>
-                            <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105"> 
-                            <a
-                            href='https://github.com/floresthomas/PI-Pokemon'
-                            target='_blank'
-                            rel='noreferrer'
-                            >Repository</a>
-                            </button>
-                        </div>
-                    </div>                
-                ))}
-                </div>
-            </div>
+  return (
+    <div
+      name="portfolio"
+      className="bg-[#47B5FF] w-full text-white md:h-screen"
+    >
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+            Portfolio
+          </p>
+          <p className="py-6">Check out some of my work right here</p>
         </div>
-    )
-}
 
-export default Projects
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
+          <div className="shadow-md shadow-gray-600 rounded-lg">
+            <Image
+              className="rounded-md duration-200 hover:scale-105"
+              src={pokemon}
+              alt="/"
+              width={500}
+              height={100}
+            />
+            <div className="flex items-center justify-center">
+              <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+                <a
+                  href="https://pokemons-react-app.netlify.app"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Deploy
+                </a>
+              </button>
+              <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+                <a
+                  href="https://github.com/floresthomas/PI-Pokemon"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Repository
+                </a>
+              </button>
+            </div>
+          </div>
+          <div className="shadow-md shadow-gray-600 rounded-lg">
+            <Image
+              className="rounded-md duration-200 hover:scale-105"
+              src={kalu}
+              alt="/"
+              width={500}
+              height={100}
+            />
+            <div className="flex items-center justify-center">
+              <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+                <a
+                  href="https://kalo-arg.vercel.app/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Deploy
+                </a>
+              </button>
+              <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+                <a onClick={() => alert("Code is private")}>Repository</a>
+              </button>
+            </div>
+          </div>
+          <div className="shadow-md shadow-gray-600 rounded-lg">
+            <Image
+              className="rounded-md duration-200 hover:scale-105"
+              src={videogames}
+              alt="/"
+              width={500}
+              height={100}
+            />
+            <div className="flex items-center justify-center">
+              <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+                <a
+                  href="https://react-videogames-app.netlify.app/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Deploy
+                </a>
+              </button>
+              <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+                <a
+                  href="https://github.com/floresthomas/PI-Videogames"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Repository
+                </a>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Projects;
